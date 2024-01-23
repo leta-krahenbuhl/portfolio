0beadb824070c6822a0d01e93dbcba26139226d9
Leta Krahenbuhl <leta.krahenbuhl@gmail.com>
2024-01-23 12:06:02 +0000

    Add fonts

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -3,7 +3,13 @@ import "./Home.scss";
 export default function Home() {
   return (
     <article className="home">
-      <header className="header"></header>
+      <header className="header">
+        <div className="header__logo">
+          <p className="header__leta">LETA</p>
+          <p className="header__krahen">KRäHENBüHL</p>
+          <p className="header__caret">^^^^^^^^^^</p>
+        </div>
+      </header>
       <article className="header2"></article>
       <nav className="nav"></nav>
       <main className="main"></main>
