c2fd601bd5e283fc3c518d3aaab2d2bc8c2ba054
Leta Krahenbuhl <leta.krahenbuhl@gmail.com>
2024-01-23 21:50:37 +0000

    Add fade in animation

--- src/Pages/About/About.js
+++ src/Pages/About/About.js
@@ -12,17 +12,16 @@ export default function About() {
 
           <p className="about__text">
             I am Leta, a recent software engineering diploma graduate. I love
-            things that make sense, ticking of lists, and playing code detective
-            following a lovely trail of error messages.
+            things that make sense, ticking off lists, and playing code
+            detective following a lovely trail of error messages.
           </p>
 
           <p className="about__text">
-            After indulging in my passion of trying to make a difference and
-            exploring the social sector whilst developing my soft and problem
+            After exploring the social sector and developing my soft and problem
             solving skills, I am going back to my roots - the tech industry.
             Coming originally from a graphic design background and now mastering
-            the technical side gives me a unique understanding of different
-            sides of the development process.
+            the technical side gives me a unique understanding of the
+            development process.
           </p>
           <p className="about__text">
             Do get in touch or check out my socials!
@@ -31,20 +30,25 @@ export default function About() {
           <a
             className="about__text-link"
             href="https://www.linkedin.com/in/leta-krahenbuhl/"
+            target="_blank"
+            rel="noreferrer"
           >
-            {" "}
-            &#128391; LinkedIn{" "}
+            &#128391; LinkedIn
           </a>
 
           <a
             className="about__text-link"
             href="https://github.com/leta-krahenbuhl"
+            target="_blank"
+            rel="noreferrer"
           >
-            {" "}
             &#128187; GitHub
           </a>
 
-          <a className="about__text-link" href="mailto:example@gmail.com">
+          <a
+            className="about__text-link"
+            href="mailto:leta.krahenbuhl@gmail.com"
+          >
             &#x1F4E7; Send me an email
           </a>
         </main>
