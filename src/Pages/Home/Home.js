93cd82fe87a84bf63a468466423fe5268bac76db
Leta Krahenbuhl <leta.krahenbuhl@gmail.com>
2024-01-23 11:30:27 +0000

    Set up app

---
+++ src/Pages/Home/Home.js
@@ -0,0 +1,12 @@
+import "./Home.scss";
+
+export default function Home() {
+  return (
+    <article className="home">
+      <header className="header"></header>
+      <article className="header2"></article>
+      <nav className="nav"></nav>
+      <main className="main"></main>
+    </article>
+  );
+}
