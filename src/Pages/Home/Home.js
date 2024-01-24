b09e201046f2856e926ca9d044d01df0f99153c1
Leta Krahenbuhl <leta.krahenbuhl@gmail.com>
2024-01-24 13:00:06 +0000

    Add mobile navigation component

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,12 +1,18 @@
 import Header from "../../components/Header/Header";
 import Navigation from "../../components/Navigation/Navigation";
+import NavigationMobile from "../../components/NavigationMobile/NavigationMobile";
 import "./Home.scss";
 
 export default function Home() {
   return (
     <main className="home">
       <Header />
-      <Navigation />
+      <div className="navigation-tablet">
+        <Navigation />
+      </div>
+      <div className="navigation-mobile">
+        <NavigationMobile />
+      </div>
     </main>
   );
 }
