19a8840912c1f4034fb89de7de6f0c4975a53b2d
Leta Krahenbuhl <leta.krahenbuhl@gmail.com>
2024-01-25 16:09:45 +0000

    Put window size event listener in about2, so HeaderSub only renders on mobile in about2

--- src/Pages/About2/About2.js
+++ src/Pages/About2/About2.js
@@ -4,12 +4,29 @@ import HeaderSub from "../../components/HeaderSub/HeaderSub";
 import Navigation from "../../components/Navigation/Navigation";
 import NavigationMobile from "../../components/NavigationMobile/NavigationMobile";
 import "./About2.scss";
+import React, { useEffect, useState } from "react";
 
 export default function About2() {
+  const [isVisible, setIsVisible] = useState(true);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsVisible(window.innerWidth < 768);
+    };
+
+    handleResize();
+
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
   return (
     <main className="home">
       <Header />
-      <HeaderSub />
+      {isVisible && <HeaderSub />}
       <div className="desktop-text-box">
         <DesktopTextBox />
       </div>
--- src/components/HeaderSub/HeaderSub.js
+++ src/components/HeaderSub/HeaderSub.js
@@ -1,24 +1,7 @@
 import "./HeaderSub.scss";
-import React, { useEffect, useState } from "react";
 
 export default function HeaderSub() {
-  const [isVisible, setIsVisible] = useState(true);
-
-  useEffect(() => {
-    const handleResize = () => {
-      setIsVisible(window.innerWidth < 768);
-    };
-
-    handleResize();
-
-    window.addEventListener("resize", handleResize);
-
-    return () => {
-      window.removeEventListener("resize", handleResize);
-    };
-  }, []);
-
-  return isVisible ? (
+  return (
     <article className="header-part2">
       <p className="header-part2__junior">JUNiOR</p>
       <p className="header-part2__software">SOFTWARE</p>
@@ -27,5 +10,5 @@ export default function HeaderSub() {
       <p className="header-part2__london">LONDON</p>
       <p className="header-part2__caret3">^^^^^^^^^^</p>
     </article>
-  ) : null;
+  );
 }
