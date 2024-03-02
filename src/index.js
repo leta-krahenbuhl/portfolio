56d56c6daf89702a65281ca854c365ae4631ae8a
Leta Krahenbuhl <leta.krahenbuhl@gmail.com>
2024-03-02 17:00:15 +0000

    Del ete comments, add Courier as fallback font in index.scss

--- src/index.js
+++ src/index.js
@@ -20,10 +20,3 @@ createRoot(document.getElementById("root")).render(
     <FontLoader />
   </React.StrictMode>
 );
-
-// const root = ReactDOM.createRoot(document.getElementById("root"));
-// root.render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>
-// );
