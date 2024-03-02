6a2d8ea7e4169df8c5d549a2afa01140f3c29237
Leta Krahenbuhl <leta.krahenbuhl@gmail.com>
2024-03-02 16:41:30 +0000

    Edite index.js to fix font loading issue

--- src/index.js
+++ src/index.js
@@ -1,11 +1,29 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
+import React, { useEffect, useState } from "react";
 import "./index.scss";
 import App from "./App";
+import { createRoot } from "react-dom/client";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
+const FontLoader = () => {
+  const [fontsLoaded, setFontsLoaded] = useState(false);
+
+  useEffect(() => {
+    document.fonts.ready.then(() => {
+      setFontsLoaded(true);
+    });
+  }, []);
+
+  return fontsLoaded ? <App /> : null;
+};
+
+createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <FontLoader />
   </React.StrictMode>
 );
+
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
